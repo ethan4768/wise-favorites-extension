55b928030a3b9e75ec09ee8be98d81edc83a8fde
ethan4768 <finengine.tech@gmail.com>
2024-12-05 20:03:49 +0800

    feat: eemove options parameter when calling wise-favorites-worker

--- lib/request.ts
+++ lib/request.ts
@@ -27,7 +27,7 @@ export const sendToWiseFavoritesWorker = async (
       .post(channel.properties["Base Path"].value)
       .type("json")
       .set("Authorization", `Bearer ${bearToken}`)
-      .send({ ...properties, content: content, options: { arsp: false, share: { github: true, telegram: false } } })
+      .send({ ...properties, content: content, options: { arsp: false } })
 
     if (!res.ok) {
       return `Cannot send request to ${basePath}:\n ${res.body.msg}`
