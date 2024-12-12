41395a1c3b402c0b36ea8411421760e17ef8c36a
ethan4768 <finengine.tech@gmail.com>
2024-12-12 13:45:42 +0800

    build: remove scripting from permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "wxt"
 
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
   extensionApi: "chrome",
   manifest: {
     name: "wise favorites extension",
-    permissions: ["activeTab", "scripting", "storage"],
+    permissions: ["activeTab", "storage"],
     action: {}
   }
 })
